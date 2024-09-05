e64d26972fbce0a421df1896d5320fca54994d87
Renato Oliveira <prof.renato.oliveira2023@gmail.com>
2024-09-05 11:24:41 -0300

    fazendo a conexão com o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { DatabaseModel } from './model/DatabaseModel';
 
 // definindo a porta do servidor
 const port: number = 3333;
@@ -11,14 +12,22 @@ server.use(express.json());
 
 // rotas da aplicação
 server.get('/', (req, res) => {
-    res.json({ mensagem: "Olá mundo, está é minha primeira aplicação web"});
+    res.json({ mensagem: "Olá mundo, está é minha primeira aplicação web" });
 });
 
 server.get('/pessoas', (req, res) => {
-    res.json({ mensagem: "Aqui será apresentado os dados das pessoas."});
+    res.json({ mensagem: "Aqui será apresentado os dados das pessoas." });
 });
 
-// iniciando o servidor
-server.listen(port, () => {
-    console.log(`Servidor iniciado no endereço http://localhost:${port}`);
+new DatabaseModel().testeConexao().then((resdb) => {
+    if (resdb) {
+        console.clear();
+        console.log("Conexão com banco de dados realizada com sucesso!");
+        // iniciando o servidor
+        server.listen(port, () => {
+            console.log(`Servidor iniciado no endereço http://localhost:${port}`);
+        });
+    } else {
+        console.log("Erro ao conectar com o banco de dados");
+    }
 });
